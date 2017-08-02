e08f476e4e33f7af0b9871a5644c852dfa0176f8
Artishina <artive112@yandex.ru>
2017-08-02 22:52:55 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+﻿var link = document.querySelector(".feedback-btn");
+var popup = document.querySelector(".feedback");
+
+var close = document.querySelector(".feedback-btn");
+
+link.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.add("feedback-show");
+
+});
+
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+  if (popup.classList.contains("feedback-show")) {
+  popup.classList.remove("feedback-show");
+  }
+  }
+});
+
+
+
+
+
